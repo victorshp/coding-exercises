bfb43410b722c151d12fd6d0420328516d60d817
Victor Park <victor.park@zenvia.com>
2021-12-01 14:11:07 -0300

    WIP: feat: function done but still faulty

--- 5kyu-ROT13/challenge.js
+++ 5kyu-ROT13/challenge.js
@@ -1,5 +1,19 @@
+// Making acessible to any ROT Definition
+const ROTVALUE = 13;
+const letterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 function rot13(message){
-  //your code here
+	let rotResult;
+	let rotLetter;
+	let rotLetterIndex;
+	let originalLetterIndex;
+ 	message.split('').forEach((letter) => {
+		letterIndex = letterList.indexOf(letter.toLowerCase()) + 1;
+		rotLetterIndex = (letterIndex > 26 - ROTVALUE) ? letterList.indexOf(ROTVALUE - (26 - letterIndex)) : letterList.indexOf(letterIndex + ROTVALUE);
+		rotLetter = letterList[rotLetterIndex];
+		rotResult += (letter.toLowerCase() !== letter) ? rotLetter.toUpperCase() : rotLetter
+	}); 
+	return rotResult
 }
 
 console.log("grfg" == rot13("test"));
